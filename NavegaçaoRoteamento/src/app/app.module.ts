67af48570cdb93863e70e2e9bc6bd62ce14e63e2
zecarloz <josecarlos2503@hotmail.com>
2021-05-05 21:20:30 -0300

    Update project

--- NavegaçaoRoteamento/src/app/app.module.ts
+++ NavegaçaoRoteamento/src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
-import { SobreComponent } from './institucional/sobre/sobre.component';
-import { ContatoComponent } from './institucional/contato/contato.component';
-import { rootRouterConfig } from './app.routes';
+
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
-    SobreComponent,
-    ContatoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
+    BrowserModule
   ],
-  providers: [
-    {provide: APP_BASE_HREF, useValue: '/'}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
